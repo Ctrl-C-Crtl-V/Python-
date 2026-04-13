a54851ecc6bd7fbeb06668d771371384b2d1e171
Adrian-Diaz <trony6036@gmail.com>
2026-04-13 17:30:48 -0300

    Add files via upload

---
+++ Projetos_Python/FolhaPag.py
@@ -0,0 +1,18 @@
+while True:
+    #Entrada de dados
+    salario=float(input('digite a nota 1:'))
+    salarioMinimo=float(input('digite a nota 2:'))
+
+    #processamento
+
+    salarioMinimo= 1621,00
+    
+    salarioMinimo=(round(salarioMinimo*2))
+
+    print('seu salaio é'(salario))
+
+    #saida
+
+
+
+    break
